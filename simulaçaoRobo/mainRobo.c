de8f22045b37c805251c2ff0eb82bbee94cf2651
ThiagoWesley18 <tw180896@gmail.com>
2024-03-24 13:03:51 -0400

    lab4

--- simulaçaoRobo/mainRobo.c
+++ simulaçaoRobo/mainRobo.c
@@ -9,115 +9,134 @@
 #define tempo 20.0
 #define D 0.6
 
-typedef struct {
-    Matrix matriz_estado_B;
-    Matrix matriz_estado_C;
-    Matrix matriz_estado_D;
-    Matrix matriz_estado_X;
-    Matrix matriz_u;
-    sem_t sem_produtor;
-    sem_t sem_consumidor;
-    pthread_mutex_t lock;
-} Monitor;
-Monitor monitor;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+sem_t sem_produtor;
+sem_t sem_consumidor;
+sem_t sem_saida; 
 
+// Variaveis globais
+Matrix matriz_estado_B;
+Matrix matriz_estado_C;
+Matrix matriz_estado_X;
+Matrix matriz_u;
+Matrix y;
+double tempo_saida = 0.0;
+int flag = 1;
 
-void* thread_Produtora(void* arg){
-    (void)arg;
-    for(double t = tempo_inicio; t <= tempo; t+=intervalo){
+void* thread_Saida(){
+  
+    while (flag == 1){
+        sem_wait(&sem_saida);
+        pthread_mutex_lock(&lock);
         
+        imprimi_resultado(tempo_saida, matriz_u, y);
+        
+        pthread_mutex_unlock(&lock);
+    }
+    return NULL; 
+}
+
+
+void* thread_Produtora(){
+    
+    for(double t = tempo_inicio; t <= tempo; t+=intervalo){    
         // Verifica se pode produzir, se pode, decrementa o semáforo e faz o lock dos recursos
-        sem_wait(&monitor.sem_produtor);
-        pthread_mutex_lock(&monitor.lock);
+        sem_wait(&sem_produtor);
+        pthread_mutex_lock(&lock);
 
         Array uk = calcula_sinal_de_entrada(t); 
-        monitor.matriz_u.matriz[0][0] = uk.in[0];
-        monitor.matriz_u.matriz[1][0] = uk.in[1];
-        monitor.matriz_estado_X = calcula_proximo_estado(monitor.matriz_estado_B, monitor.matriz_u);
-    
-        // Libera o lock e incrementa o semáforo consumidor para indicar que o recurso está disponível
-        pthread_mutex_unlock(&monitor.lock);
-        sem_post(&monitor.sem_consumidor);
+        matriz_u.matriz[0][0] = uk.in[0];
+        matriz_u.matriz[1][0] = uk.in[1];
+        matriz_estado_X = calcula_proximo_estado(matriz_estado_B, matriz_u);
 
-        espera_thread(30);
+        // Libera o lock e incrementa o semáforo consumidor para indicar que o recurso está disponível
+        pthread_mutex_unlock(&lock);
+        sem_post(&sem_consumidor);
     }
+    espera_thread(30);
+    
     return NULL;
 }
-void* thread_Consumidora(void*  arg){
-    (void)arg;
-    Matrix y = newMatrix(3, 1);
+void* thread_Consumidora(){
+
     for(double t = tempo_inicio; t <= tempo; t+=intervalo){
         // trava a thread até que o semáforo consumidor seja liberado pelo sem_post do produtor
-        sem_wait(&monitor.sem_consumidor);
-        pthread_mutex_lock(&monitor.lock);
+        sem_wait(&sem_consumidor);
+        pthread_mutex_lock(&lock);
 
-        integral_matriz_X(monitor.matriz_estado_X); // novo x
-        atualiza_matriz_B(monitor.matriz_estado_B, monitor.matriz_estado_X); // atualiza a matriz B com o novo x
-        atualiza_matriz_D(monitor.matriz_estado_D, monitor.matriz_estado_X, D); // atualiza a matriz D com o novo x
+        integral_matriz_X(matriz_estado_X); // calcula nova matriz X
+        atualiza_matriz_B(matriz_estado_B, matriz_estado_X); // atualiza a matriz B com o novo x
+        y = calcula_sinal_de_saida(matriz_estado_C, matriz_estado_X);
+        tempo_saida = t;
 
-        y = calcula_sinal_de_saida(monitor.matriz_estado_C, monitor.matriz_estado_X, monitor.matriz_estado_D);
-        imprimi_resultado(t, monitor.matriz_u, y);
-
+        sem_post(&sem_saida); 
         // Libera o lock e incrementa o semáforo produtor para liberar o recurso
-        pthread_mutex_unlock(&monitor.lock);
-        sem_post(&monitor.sem_produtor);
-        espera_thread(50);
+        pthread_mutex_unlock(&lock);
+        sem_post(&sem_produtor);
     }
+    espera_thread(50);
+    
     return NULL;
 }
 
 void inicializa_Matriz(){
-    sem_init(&monitor.sem_produtor, 0, 5);
-    sem_init(&monitor.sem_consumidor, 0, 0);
+    sem_init(&sem_produtor, 0, 5);
+    sem_init(&sem_consumidor, 0, 0);
+    sem_init(&sem_saida, 0, 0); 
 
-    monitor.matriz_estado_X= newMatrix(3, 1);
-    monitor.matriz_estado_B = newMatrix(3, 2);
-    monitor.matriz_estado_C = newMatrix(2, 3);
-    monitor.matriz_estado_D = newMatrix(2, 1);
+    matriz_estado_X= newMatrix(3, 1);
+    matriz_estado_B = newMatrix(3, 2);
+    matriz_estado_C = newMatrix(3, 3);
 
-    monitor.matriz_u = newMatrix(2, 1);
+    matriz_u = newMatrix(2, 1);
+    y = newMatrix(3, 1);
 
-    monitor.matriz_estado_X.matriz[0][0] = 0;
-    monitor.matriz_estado_X.matriz[1][0] = 0;
-    monitor.matriz_estado_X.matriz[2][0] = 0;
+    matriz_estado_X.matriz[0][0] = 0;
+    matriz_estado_X.matriz[1][0] = 0;
+    matriz_estado_X.matriz[2][0] = 0;
 
-    monitor.matriz_estado_B.matriz[0][0] = sin(monitor.matriz_estado_X.matriz[2][0]);
-    monitor.matriz_estado_B.matriz[0][1] = 0;
-    monitor.matriz_estado_B.matriz[1][0] = cos(monitor.matriz_estado_X.matriz[2][0]);
-    monitor.matriz_estado_B.matriz[1][1] = 0;
-    monitor.matriz_estado_B.matriz[2][0] = 0;
-    monitor.matriz_estado_B.matriz[2][1] = 1;
+    matriz_estado_B.matriz[0][0] = sin(matriz_estado_X.matriz[2][0]);
+    matriz_estado_B.matriz[0][1] = 0;
+    matriz_estado_B.matriz[1][0] = cos(matriz_estado_X.matriz[2][0]);
+    matriz_estado_B.matriz[1][1] = 0;
+    matriz_estado_B.matriz[2][0] = 0;
+    matriz_estado_B.matriz[2][1] = 1;
 
-    monitor.matriz_estado_C.matriz[0][0] = 1;
-    monitor.matriz_estado_C.matriz[0][1] = 0;
-    monitor.matriz_estado_C.matriz[0][2] = 0;
-    monitor.matriz_estado_C.matriz[1][0] = 0;
-    monitor.matriz_estado_C.matriz[1][1] = 1;
-    monitor.matriz_estado_C.matriz[1][2] = 0;
 
-    monitor.matriz_estado_D.matriz[0][0] = (D/2) * cos(monitor.matriz_estado_X.matriz[2][0]);
-    monitor.matriz_estado_D.matriz[1][0] = (D/2) * sin(monitor.matriz_estado_X.matriz[2][0]);
+    matriz_estado_C.matriz[0][0] = 0.5 * D * cos(matriz_estado_X.matriz[2][0]);
+    matriz_estado_C.matriz[0][1] = 0;
+    matriz_estado_C.matriz[0][2] = 0;
+    matriz_estado_C.matriz[1][0] = 0;
+    matriz_estado_C.matriz[1][1] = 0.5 * D * cos(matriz_estado_X.matriz[2][0]);
+    matriz_estado_C.matriz[1][2] = 0;
+    matriz_estado_C.matriz[2][0] = 0;
+    matriz_estado_C.matriz[2][1] = 0;
+    matriz_estado_C.matriz[2][2] = 1;  
 }
 
 int main(){
     pthread_t t1;
     pthread_t t2;
-
-    // Inicializa o mutex
-    pthread_mutex_init(&monitor.lock, NULL);
+    pthread_t thread_tempo;
 
     inicializa_Matriz();
- 
-    pthread_create(&t1, NULL, thread_Produtora, NULL); 
-    pthread_create(&t2, NULL, thread_Consumidora, NULL);
 
+    pthread_create(&thread_tempo, NULL, thread_Saida, NULL ); 
+    pthread_create(&t1, NULL, thread_Produtora, NULL ); 
+    pthread_create(&t2, NULL, thread_Consumidora, NULL );
+    
+    
     pthread_join(t1, NULL);
     pthread_join(t2, NULL);
+    flag = 0;
+    sem_post(&sem_saida); 
+    pthread_join(thread_tempo, NULL);
 
-    sem_destroy(&monitor.sem_produtor);
-    sem_destroy(&monitor.sem_consumidor);
+    sem_destroy(&sem_produtor);
+    sem_destroy(&sem_consumidor);
+    sem_destroy(&sem_saida);
 
-    freeMatrix(monitor.matriz_estado_B, monitor.matriz_estado_C, monitor.matriz_estado_X, monitor.matriz_u, NULL);
+    //freeMatrix(matriz_estado_B, matriz_estado_C, matriz_estado_X, matriz_u, NULL);
 
     return 0;
 }
